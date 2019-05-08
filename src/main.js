a61fe2e8692a5052e0b25335ba4c6f7f3b9d00eb
hard-bitten51 <290419089@qq.com>
2019-05-08 20:57:13 +0800

    测试修改main.js

--- src/main.js
+++ src/main.js
@@ -17,26 +17,7 @@ Vue.config.productionTip = false
 import request from './libs/http'
 Vue.use(request)
 
-//设置axios到Vue原型上   在使用声明请求时使用
-// import axios from 'axios'
-// Vue.prototype.$axios = axios
-//抽取后的axios在使用时   须将axios改写为this.$axios
-//配置全局基地址 （详解查看github文档）
-// axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/';
-// //token添加到情求头
-// axios.interceptors.request.use(
-//   config => {
-//     if (localStorage.getItem('token')) {
-//       config.headers.Authorization = localStorage.getItem('token');
-//     }
-//     return config;
-//   },
-//   error => {
-//     return Promise.reject(error);
-//   });
-
 new Vue({
   render: h => h(App),
-  //挂载router
   router
 }).$mount('#app')
